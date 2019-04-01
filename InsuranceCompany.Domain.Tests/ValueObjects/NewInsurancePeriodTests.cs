af7a27fb23916afe7a7690eecbf10887eb5ed23b
RS <rasstbiz@gmail.com>
2019-04-01 17:41:55 +0200

    added missing unit tests for NewInsurancePeriod

---
+++ InsuranceCompany.Domain.Tests/ValueObjects/NewInsurancePeriodTests.cs
@@ -0,0 +1,36 @@
+﻿using AutoFixture;
+using Domain;
+using System;
+using Xunit;
+
+namespace InsurancyCompany.Domain.Tests
+{
+    public class NewInsurancePeriodShould
+    {
+        private readonly IFixture fixture;
+
+        public NewInsurancePeriodShould()
+        {
+            fixture = new Fixture()
+                .Customize(new SupportMutableValueTypesCustomization());
+        }
+
+        [Fact]
+        public void ThrowWhenValidFromDateIsInThePast_ValidMonthsConstructor()
+        {
+            var validFromInThePast = DateTime.Now.AddDays(-1);
+
+            Assert.Throws<InvalidInsurancePeriodException>(() =>
+                new NewInsurancePeriod(validFromInThePast, fixture.Create<short>()));
+        }
+
+        [Fact]
+        public void ThrowWhenValidFromDateIsInThePast_ValidTillConstructor()
+        {
+            var validFromInThePast = DateTime.Now.AddDays(-1);
+
+            Assert.Throws<InvalidInsurancePeriodException>(() =>
+                new NewInsurancePeriod(validFromInThePast, DateTime.Now.AddDays(1)));
+        }
+    }
+}
